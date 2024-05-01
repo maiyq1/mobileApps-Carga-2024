c4ea656b6c8fcaa534cccb913fd461c575d0c13f
Mijael Yen <mijael.yq2002@gmail.com>
2024-04-30 23:59:24 -0500

    screens login and signup

--- app/src/main/java/com/example/projectcarga/SignupScreen.kt
+++ app/src/main/java/com/example/projectcarga/SignupScreen.kt
@@ -2,23 +2,22 @@ package com.example.projectcarga
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginScreen : AppCompatActivity() {
+class SignupScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_screen)
+        setContentView(R.layout.activity_signup_screen)
 
-        val btnSignupRedirect : TextView = findViewById(R.id.signupRedirect)
+        val btnLoginRedirect : TextView = findViewById(R.id.loginRedirect)
 
-        btnSignupRedirect.setOnClickListener() {
-            val i = Intent(this, SignupScreen::class.java)
+        btnLoginRedirect.setOnClickListener() {
+            val i = Intent(this, LoginScreen::class.java)
             startActivity(i)
         }
     }
